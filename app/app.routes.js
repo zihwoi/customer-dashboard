c577549d74f96bd4fa6dd8a13b8a67381f961627
zihwoi <72529512+zihwoi@users.noreply.github.com>
2025-01-17 12:09:46 +0800

    set up routes and modules, dashboard

---
+++ app/app.routes.js
@@ -0,0 +1,14 @@
+angular
+    .module('customerApp')
+    .config(['$routeProvided', function($routeProvider) {
+        $routeProvider
+            .when('/', {    
+                templateUrl: 'app/views/dashboard.html',
+                controller: 'MainController'
+            })
+            .when('/customers', {
+                templateUrl: 'app/views/customer-list.html',
+                controller: 'CustomerController'
+            })
+            .otherwise({redirectTo: '/'});
+    }]);
